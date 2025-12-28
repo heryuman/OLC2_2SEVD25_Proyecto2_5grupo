db98da6d21981e5bda89dd5feab5433182a015c3
OLD <old@192.168.1.14>
2025-12-27 19:02:00 -0600

    limpieza de datos numericos

--- Back/app/routes/file_routes.py
+++ Back/app/routes/file_routes.py
@@ -37,19 +37,83 @@ def limpiar():
     csv_path = os.path.join(UPLOAD_DIR, "insight_cluster.csv")
 
     try:
+        columns_num = [
+            'cliente_id',
+            'frecuencia_compra',
+            'monto_total_gastado',
+            'monto_promedio_compra',
+            'dias_desde_ultima_compra',
+            'antiguedad_cliente_meses',
+            'numero_productos_distintos',
+            'reseña_id'
+        ]
         pd.set_option('display.max_columns', None)
         df = pd.read_csv(csv_path)
 
         print("**************************************************************")
         print("*******************INICIA CARGA*******************************")
         print("**************************************************************")
-
+        print(df.head())
+        print("**************************************************************")
+        print("**************************************************************")
+        df.info()
         #Eliminamos registros con valores nulos
         df = df.dropna(subset=['cliente_id'])
         #Buscamos registros duplicados
         duplicados = df[df.duplicated(subset=['cliente_id'], keep=False)]
         #Eliminamos registros duplicados
         df = df.drop_duplicates(subset=['cliente_id'], keep='first')
+
+
+        #LIMPIEZA DATA DE TIPO NUMERICO
+
+        for column in columns_num:
+            
+            # 1️⃣ Validar que sea numérico real (NO limpiar texto raro)
+            mask_numeric = df[column].astype(str).str.match(r'^\d+(\.\d+)?$')
+
+            # 2️⃣ Eliminar filas inválidas
+            df = df[mask_numeric]
+
+            # 3️⃣ Convertir a numérico
+            df[column] = df[column].astype(float)
+
+            
+
+            # 1. Limpiar rango
+            #df = df[(df[column] >= 0) & (df[column] <= 100)]
+
+            # 2. Outliers
+            Q1 = df[column].quantile(0.25)
+            Q3 = df[column].quantile(0.75)
+            IQR = Q3 - Q1
+
+            lower = Q1 - 1.5 * IQR
+            upper = Q3 + 1.5 * IQR
+
+            # 3. Filtrar
+            df = df[(df[column] >= lower) & (df[column] <= upper)]
+
+            # 4. Imputar con median
+            df[column] = df[column].fillna(
+                df[column].median()
+            )
+
+
+            hay_decimal = (df[column] % 1 != 0).any()
+
+            if hay_decimal:
+                print("hay decimales")
+            else:
+                df[column] = df[column].astype("int64")
+
+        print("**************************************************************")
+        print("*********************CON CORRECCIONES*************************")
+        print(df.head())
+        print("**************************************************************")
+        print("**************************************************************")
+        df.info()
+
 
         ruta = 'clean_files/data_clean.csv'
 
